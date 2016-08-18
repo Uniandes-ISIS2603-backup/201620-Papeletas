5315a902a159a47324e5c49ff420998dc6b13d89
ANGEL CAMILO CABEZAS VARELA <ac.cabezas716@sis.virtual.uniandes.edu.co>
2016-08-17 21:19:20 -0500

    PathParam

--- src/main/java/co/edu/uniandes/rest/hospital/resources/HorarioResource.java
+++ src/main/java/co/edu/uniandes/rest/hospital/resources/HorarioResource.java
@@ -13,6 +13,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 /**
@@ -30,7 +31,8 @@ public class HorarioResource {
     }
     
     @POST
-    public HorarioDTO createHorario(int pMes, int pAño){
+    @Path("{pMes: \\d+}/{pAño: \\d+}")
+    public HorarioDTO createHorario(@PathParam("pMes")int pMes,@PathParam("pAño")int pAño){
         return horarioLogica.crearHorario(pMes, pAño);
     }
     
