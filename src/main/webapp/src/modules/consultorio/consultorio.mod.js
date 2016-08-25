fff4e14766daed7f9ddf5355165775140e82515d
Juan Felipe Méndez <jf.mendez11@uniandes.edu.co>
2016-08-25 11:49:35 -0500

    Moduloo Consultorio modificacion

--- src/main/webapp/src/modules/consultorio/consultorio.mod.js
+++ src/main/webapp/src/modules/consultorio/consultorio.mod.js
@@ -3,4 +3,4 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var mod = ng.module("consultorioModule", ["ngMessages"]);
+var mod = ng.module("consultorioModule", ["ui-router"]);
