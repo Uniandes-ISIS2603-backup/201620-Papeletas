bae84427d60fd26b1507af1332a17ae80c78ffeb
diegocastrolosada <df.castro12@uniandes.edu.co>
2016-08-29 21:36:58 -0500

    Paciente module progreso

--- src/main/webapp/src/modules/paciente/paciente.mod.js
+++ src/main/webapp/src/modules/paciente/paciente.mod.js
@@ -3,5 +3,40 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
+var mod = ng.module("pacienteModule",["ui-router"]);
+mod.constant("pacienteContext","api/pacientes");
+mod.config(['$stateProvider','$urlRouterProvider', function ($stateProvider, $urlRouterProvider){
+        var basePath = 'src/modules/paciente/';
+        $urlRouterProvider.otherwise("/pacientesList");
+        $stateProvider.state('pacientesList',{
+            url: '/pacientes',
+            views: {
+                'mainView': {
+                    controller: 'pacientesCtrl',
+                    controllerAs: 'ctrl',
+                    templateUrl: basePath + 'pacientes.list.html'
+                }
+            }
+        }).state('pacienteCreate',{
+            url: '/pacientes/create',
+            views: {
+                'mainView': {
+                    controller: 'pacientesCtrl',
+                    controllerAs: 'ctrl',
+                    templateUrl: basePath + 'pacientes.create.html'
+                }
+            }
+        }).state('pacienteEdit',{
+            url: '/pacientes/:pacienteId',
+            param:{
+                pacienteId: null
+            },
+            views: {
+                'mainView': {
+                    controller: 'pacientesCtrl',
+                    controllerAs: 'ctrl',
+                    templateUrl: basePath + 'pacientes.create.html'
+                }
+            }
+        })
+}])
