e2abae21d8a17bd950dd78e3adda83546b3ee506
Juan Felipe Méndez <jf.mendez11@uniandes.edu.co>
2016-08-29 22:24:14 -0500

    Moduloo Consultorio modificacion

---
+++ src/main/webapp/src/modules/consultorio/consultorio.ctrl.js
@@ -0,0 +1,89 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+(function (ng) {
+    var mod = ng.module("consultorioModule");
+
+    mod.controller("consultorioCtrl", ['$scope', '$state', '$stateParams', '$http', 'consultorioContext', function ($scope, $state, $stateParams, $http, context) {
+            
+            $scope.records = {};
+            
+            $http.get(context).then(function (response){
+                $scope.records = response.data;
+            },responseError);
+            
+            
+            
+            if ($stateParams.consultorioId !== null && $stateParams.consultorioId !== undefined) {
+                
+                id = $stateParams.consultorioId;
+                
+                $http.get(context + "/" + id).then(function(response) {
+                     $scope.currentRecord = response.data;
+                }, responseError);
+            } else {
+                
+                $scope.currentRecord = {
+                    id: undefined,
+                    libre: false
+                };
+                
+                $scope.alerts = [];
+            }
+                        
+            this.saveRecord = function (id) {
+                
+                currentRecord = $scope.currentRecord;
+                
+                if (id == null) {
+                    return $http.post(context, currentRecord).then(function() {
+                        $state.go("consultorioList");
+                    }, responseError);
+                }
+                else {
+                    return $http.put(context + "/" + currentRecord.id, currentRecord).then(function () {
+                        $state.go("consultorioList");
+                    }, responseError);
+                };
+                
+            };
+            
+            this.removeRecord = function (id) {
+                if (id !== null) {
+                    $http.delete(context + "/" + $scope.currentRecord.id, $scope.currentRecord).then(function () {
+                        $state.go("consultorioList");
+                    }, responseError);
+                };
+            };
+            
+            this.closeAlert = function (index) {
+                $scope.alerts.splice(index, 1);
+            };
+
+            function showMessage(msg, type) {
+                var types = ["info", "danger", "warning", "success"];
+                if (types.some(function (rc) {
+                    return type === rc;
+                })) {
+                    $scope.alerts.push({type: type, msg: msg});
+                }
+            }
+
+            this.showError = function (msg) {
+                showMessage(msg, "danger");
+            };
+
+            this.showSuccess = function (msg) {
+                showMessage(msg, "success");
+            };
+
+            var self = this;
+            function responseError(response) {
+
+                self.showError(response.data);
+            }
+    }]);
+})(window.angular);
--- src/main/webapp/src/modules/consultorio/consultorio.mod.js
+++ src/main/webapp/src/modules/consultorio/consultorio.mod.js
@@ -3,4 +3,55 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var mod = ng.module("consultorioModule", ["ui-router"]);
+(function (ng) {
+    var mod = ng.module("consultorioModule", ["ui-router"]);
+
+    mod.config(['$stateProvider','$urlRouterProvider',function($stateProvider,$urlRouterProvider){
+        var basePath = 'src/modules/consultorio';
+        $urlRouterProvider.otherwise("/consultorioList");
+    
+        $stateProvider.state("consultorioList", {
+            url: "/consultorio",
+            views: {
+                "mainView" : {
+                  controller : "consultoriosCtrl",
+                  controllerAs: "ctrl",
+                  templateURL: basePath + "consultorio.list.html"
+                }
+            }
+        }).state("consultorioCreate", {
+            url: "/consultorio/create",
+            views: {
+                "mainView" : {
+                    controller : "consultoriosCtrl",
+                    controllerAs: "ctrl",
+                    templateURL: basePath + "consultorio.create.html"
+                }
+            }
+        }).state("consultorioDelete", {
+            url: "/consultorio/:consultorioId",
+            param: {
+                consultorioId : null
+            },
+            views: {
+                "mainView" : {
+                    controller : "consultoriosCtrl",
+                    controllerAs : "ctrl",
+                    templateURL : basePath + "consultorio.list.html"
+                }
+            }
+        }).state("consultorioEdit", {
+            url: "/consultorio/:consultorioId",
+            param: {
+                consultorioId : null
+            },
+            views : {
+                "mainView" : {
+                    controller : "consultoriosCrtl",
+                    controllerAs : "ctrl",
+                    templateURL : basePath + "consultorio.create.html"
+                }
+            }
+        }) 
+    }]);
+})(window.angular);
