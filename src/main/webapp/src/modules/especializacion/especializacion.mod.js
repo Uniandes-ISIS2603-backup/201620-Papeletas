f300c7456c6da3e264f9b551f00219aa0dff399d
Juan Camilo Lara <jc.lara10@uniandes.edu.co>
2016-08-30 09:18:53 -0500

    Especializacion mod

---
+++ src/main/webapp/src/modules/especializacion/especializacion.mod.js
@@ -0,0 +1,24 @@
+(function (ng)
+{
+    var mod = ng.module("especializacionModule", []);
+    mod.constant("especializacion", "api/especializacion");
+    mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider)
+    {
+        var basePath = 'src/modules/especializacion';
+        $urlRouterProvider.otherwise("/especializacionList");
+        $stateProvider.state('especializacionList',
+        {
+            url: '/especializacion', views:
+                    {
+                        'mainView':{controller:'specCTRL', controllerAs:'CTRL', templateUrl: basePath + 'especializacion.list.html'}
+                    }
+        }).state('especializacionCreate',
+        {
+                url: '/especializacion/create',
+                views: 
+                {
+                    'mainView': {controller: 'citiesCtrl', controllerAs: 'ctrl', templateUrl: basePath + 'cities.create.html'}
+                }
+        })
+    }])
+})(window.angular);
