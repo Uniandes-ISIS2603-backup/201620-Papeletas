d0cf7f0f2aca4598b97da83ac3f91cb4a50a0520
jc.useche10 <jc.useche10@ISIS2603-12.sis.virtual.uniandes.edu.co>
2016-08-31 10:20:41 -0500

    ctrl horario ya no arroja error

--- src/main/webapp/src/modules/horario/horario.ctrl.js
+++ src/main/webapp/src/modules/horario/horario.ctrl.js
@@ -1,14 +1,103 @@
-var mod = ng.module("horarioModule");
+(function (ng) {
+    var mod = ng.module("horarioModule");
 
-mod.controller('horarioController', ['$scope', '$state', '$stateParams', '$http', function ($scope, $state, $stateParams, $http) {
-        this.listaHorarios = function () {
-            $scope.horarios = {};
-            $http.get('http://localhost:8080/Papeletas/api/horario').
-                    then(function (response) {
-                        $scope.horarios = response.data;
-                    }),
-                    then(function (response) {
-                        //TODO: si algo sale mal
-                    });
-        }
-    }]);
+    mod.controller("horarioCtrl", ['$scope', '$state', '$stateParams', '$http', 'horarioContext', function ($scope, $state, $stateParams, $http, context) {
+
+            // inicialmente el listado de horario está vacio
+            $scope.records = {};
+            // carga las horario
+            $http.get(context).then(function(response){
+                $scope.records = response.data;    
+            }, responseError);
+
+            // el controlador recibió un horarioId ??
+            // revisa los parámetros (ver el :horarioId en la definición de la ruta)
+            if ($stateParams.horarioId !== null && $stateParams.horarioId !== undefined) {
+                
+                // toma el id del parámetro
+                id = $stateParams.horarioId;
+                // obtiene el dato del recurso REST
+                $http.get(context + "/" + id)
+                    .then(function (response) {
+                        // $http.get es una promesa
+                        // cuando llegue el dato, actualice currentRecord
+                        $scope.currentRecord = response.data;
+                    }, responseError);
+
+            // el controlador no recibió un horarioId
+            } else
+            {
+                // el registro actual debe estar vacio
+                $scope.currentRecord = {
+                    id: undefined /*Tipo Long. El valor se asigna en el backend*/,
+                    name: '' /*Tipo String*/,
+                };
+              
+                $scope.alerts = [];
+            }
+
+
+            this.saveRecord = function (id) {
+                currentRecord = $scope.currentRecord;
+                
+                // si el id es null, es un registro nuevo, entonces lo crea
+                if (id == null) {
+
+                    // ejecuta POST en el recurso REST
+                    return $http.post(context, currentRecord)
+                        .then(function () {
+                            // $http.post es una promesa
+                            // cuando termine bien, cambie de estado
+                            $state.go('horarioList');
+                        }, responseError);
+                        
+                // si el id no es null, es un registro existente entonces lo actualiza
+                } else {
+                    
+                    // ejecuta PUT en el recurso REST
+                    return $http.put(context + "/" + currentRecord.id, currentRecord)
+                        .then(function () {
+                            // $http.put es una promesa
+                            // cuando termine bien, cambie de estado
+                            $state.go('horarioList');
+                        }, responseError);
+                };
+            };
+
+
+
+            // -----------------------------------------------------------------
+            // Funciones para manejra los mensajes en la aplicación
+
+
+            //Alertas
+            this.closeAlert = function (index) {
+                $scope.alerts.splice(index, 1);
+            };
+
+            // Función showMessage: Recibe el mensaje en String y su tipo con el fin de almacenarlo en el array $scope.alerts.
+            function showMessage(msg, type) {
+                var types = ["info", "danger", "warning", "success"];
+                if (types.some(function (rc) {
+                    return type === rc;
+                })) {
+                    $scope.alerts.push({type: type, msg: msg});
+                }
+            }
+
+            this.showError = function (msg) {
+                showMessage(msg, "danger");
+            };
+
+            this.showSuccess = function (msg) {
+                showMessage(msg, "success");
+            };
+
+            var self = this;
+            function responseError(response) {
+
+                self.showError(response.data);
+            }
+        }]);
+
+})(window.angular);
