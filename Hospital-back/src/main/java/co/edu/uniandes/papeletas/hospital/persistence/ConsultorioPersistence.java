fb15c2f113011abfd567c080fb9e160ccca3fd88
Juan Felipe Méndez <jf.mendez11@uniandes.edu.co>
2016-10-06 12:13:37 -0500

    Cambios en persistence.xml y ConsultorioPersistenceTest

--- Hospital-back/src/main/java/co/edu/uniandes/papeletas/hospital/persistence/ConsultorioPersistence.java
+++ Hospital-back/src/main/java/co/edu/uniandes/papeletas/hospital/persistence/ConsultorioPersistence.java
@@ -5,10 +5,24 @@
  */
 package co.edu.uniandes.papeletas.hospital.persistence;
 
+import co.edu.uniandes.papeletas.hospital.entities.ConsultorioEntity;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 /**
  *
  * @author jf.mendez11
  */
+@Stateless
 public class ConsultorioPersistence {
+    
+    @PersistenceContext(unitName = "PapeletasPU")
+    protected EntityManager em;
+    
+    public ConsultorioEntity find (Long id) {
+        return em.find(ConsultorioEntity.class, id);
+    }
+    
     
 }
---
+++ Hospital-back/src/test/java/co/edu/uniandes/papeletas/hospital/persistence/ConsultorioPersistenceTest.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.papeletas.hospital.persistence;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jf.mendez11
+ */
+public class ConsultorioPersistenceTest {
+    
+    public ConsultorioPersistenceTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of find method, of class ConsultorioPersistence.
+     */
+    @Test
+    public void testFind() throws Exception {
+    }
+    
+}
