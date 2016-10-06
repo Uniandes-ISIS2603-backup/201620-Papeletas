e3b4523c324f2dd0ba896bcd4dfa216af80bf0c8
Juan Felipe Méndez <jf.mendez11@uniandes.edu.co>
2016-10-06 11:45:29 -0500

    Persistence package

---
+++ Hospital-back/src/main/java/co/edu/uniandes/papeletas/hospital/entities/ConsultorioEntity.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.papeletas.hospital.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author jf.mendez11
+ */
+@Entity
+public class ConsultorioEntity extends BaseEntity implements Serializable {
+    
+}
