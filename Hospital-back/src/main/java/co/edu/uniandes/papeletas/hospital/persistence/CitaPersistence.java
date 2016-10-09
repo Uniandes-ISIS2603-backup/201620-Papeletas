ed8910154bbe759b3bb47f0cebac0afe9f6dd82e
jc.useche10 <jc.useche10@ISIS2603-12.sis.virtual.uniandes.edu.co>
2016-10-09 13:03:46 -0500

    CitaEntity y CitaPersistence, habra errores hasta que se creen los Entity que faltan

--- Hospital-back/src/main/java/co/edu/uniandes/papeletas/hospital/persistence/CitaPersistence.java
+++ Hospital-back/src/main/java/co/edu/uniandes/papeletas/hospital/persistence/CitaPersistence.java
@@ -5,7 +5,15 @@
  */
 package co.edu.uniandes.papeletas.hospital.persistence;
 
+import co.edu.uniandes.papeletas.hospital.entities.CitaEntity;
+import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 /**
  *
@@ -17,4 +25,41 @@ public class CitaPersistence {
 
     @PersistenceContext(unitName = "PapeletasPU")
      protected EntityManager em;
+    
+        public CitaEntity find(Long id) {
+        LOGGER.log(Level.INFO, "Consultando  cita con id={0}", id);
+        return em.find(CitaEntity.class, id);
+    }
+
+    public CitaEntity findByName(String name) {
+        LOGGER.log(Level.INFO, "Consultando  cita con name= ", name);
+        TypedQuery<CitaEntity> q
+                = em.createQuery("select u from CitaEntity u where u.name = :name", CitaEntity.class);
+        q = q.setParameter("name", name);
+        return q.getSingleResult();
+    }
+
+    public List<CitaEntity> findAll() {
+        LOGGER.info("Consultando todos los  citas");
+        Query q = em.createQuery("select u from CitaEntity u");
+        return q.getResultList();
+    }
+
+    public CitaEntity create(CitaEntity entity) {
+        LOGGER.info("Creando un  cita nuevo");
+        em.persist(entity);
+        LOGGER.info("Cita creado");
+        return entity;
+    }
+
+    public CitaEntity update(CitaEntity entity) {
+        LOGGER.log(Level.INFO, "Actualizando  cita con id={0}", entity.getId());
+        return em.merge(entity);
+    }
+
+    public void delete(Long id) {
+        LOGGER.log(Level.INFO, "Borrando  cita con id={0}", id);
+        CitaEntity entity = em.find(CitaEntity.class, id);
+        em.remove(entity);
+    }
 }
