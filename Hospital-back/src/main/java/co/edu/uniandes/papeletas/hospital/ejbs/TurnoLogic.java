544a0cb0708b60c9297e7eb2aee4c60b6923eba8
ANGEL CAMILO CABEZAS VARELA <ac.cabezas716@sis.virtual.uniandes.edu.co>
2016-11-03 06:48:41 -0500

    turno logic implementacion

--- Hospital-back/src/main/java/co/edu/uniandes/papeletas/hospital/ejbs/TurnoLogic.java
+++ Hospital-back/src/main/java/co/edu/uniandes/papeletas/hospital/ejbs/TurnoLogic.java
@@ -9,11 +9,14 @@ import co.edu.uniandes.papeletas.hospital.api.IConsultorioLogic;
 import co.edu.uniandes.papeletas.hospital.api.IMedicoLogic;
 import co.edu.uniandes.papeletas.hospital.api.ITurnoLogic;
 import co.edu.uniandes.papeletas.hospital.entities.ConsultorioEntity;
+import co.edu.uniandes.papeletas.hospital.entities.MedicoEntity;
 import co.edu.uniandes.papeletas.hospital.entities.TurnoEntity;
 import co.edu.uniandes.papeletas.hospital.exceptions.HospitalLogicException;
 import co.edu.uniandes.papeletas.hospital.persistence.TurnoPersistence;
+import java.util.Calendar;
 import java.util.List;
 import javax.inject.Inject;
+import javax.persistence.NoResultException;
 
 /**
  *
@@ -32,37 +35,68 @@ public class TurnoLogic implements ITurnoLogic {
 
     @Override
     public List<TurnoEntity> getTurnos(Long pMedicoId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        MedicoEntity medico = medicoLogic.getMedico(pMedicoId);
+        return medico.turnos();
     }
 
     @Override
     public TurnoEntity getTurno(Long pId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+        return persistence.find(pId);
+          } catch (NoResultException e) {
+            throw new IllegalArgumentException("El turno no existe");
+        }
     }
 
     @Override
-    public TurnoEntity getTurnoByName(Long pMedicoId, String pName) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public TurnoEntity getTurnoByName(String pName) {
+        return persistence.findByName(pName);
     }
 
     @Override
     public TurnoEntity createTurno(Long pMedicoId, TurnoEntity pEntityTurno) throws HospitalLogicException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        TurnoEntity alreadyExist = getTurnoByName(pEntityTurno.getName());
+        Calendar c1 = Calendar.getInstance();
+        c1.setTime(pEntityTurno.getFecha());
+        //fecha actual
+        Calendar c2 = Calendar.getInstance();
+        if (alreadyExist != null) 
+        {
+            throw new HospitalLogicException("Ya existe un turno con el mismo nombre en el hospital");
+        } 
+        if(c1.before(c2))
+        {
+            throw new HospitalLogicException("El turno no puede tener una fecha pasada");
+        }
+        if(pEntityTurno.getDuracion()<0)
+        {
+            throw new HospitalLogicException("Ya duracion del turno no puede ser menor a 0 horas.");
+        }
+       
+            MedicoEntity  medico = medicoLogic.getMedico(pMedicoId);
+            pEntityTurno.setMedico(medico);
+
+            pEntityTurno = persistence.create(pEntityTurno);
+        
+        return pEntityTurno;
     }
 
     @Override
     public TurnoEntity updateTurno(Long pMedicoId, TurnoEntity pEntityTurno) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        MedicoEntity medico = medicoLogic.getMedico(pMedicoId);
+        pEntityTurno.setMedico(medico);
+        return persistence.update(pEntityTurno);
     }
 
     @Override
     public void deleteTurno(Long pId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        TurnoEntity old = getTurno(pId);
+        persistence.delete(old.getId());
     }
 
     @Override
     public ConsultorioEntity getConsultorioTurno(Long pId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return persistence.find(pId).getConsultorio();  
     }
     
 }
