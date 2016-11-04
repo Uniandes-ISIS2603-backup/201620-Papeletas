c52b3f4211462a03c57762e1a25ef57bf293a7dd
jclara10 <jc.lara10@uniandes.edu.co>
2016-11-04 13:24:25 -0500

    correccion resource

--- Hospital-web/src/main/java/co/edu/uniandes/rest/hospital/resources/EspecializacionResource.java
+++ Hospital-web/src/main/java/co/edu/uniandes/rest/hospital/resources/EspecializacionResource.java
@@ -1,5 +1,8 @@
 package co.edu.uniandes.rest.hospital.resources;
 
+import co.edu.uniandes.papeletas.hospital.api.IEspecializacionLogic;
+import co.edu.uniandes.papeletas.hospital.ejbs.EspecializacionLogic;
+import co.edu.uniandes.papeletas.hospital.entities.EspecializacionEntity;
 import java.util.List;
 
 import javax.ws.rs.DELETE;
@@ -10,9 +13,11 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
-import co.edu.uniandes.rest.hospital.mocks.EspecializacionMock;
+//import co.edu.uniandes.rest.hospital.mocks.EspecializacionMock;
 import co.edu.uniandes.rest.hospital.dtos.EspecializacionDTO;
+import co.edu.uniandes.rest.hospital.dtos.EspecializacionDetailDTO;
 import co.edu.uniandes.rest.hospital.exceptions.EspecializacionException;
+import javax.inject.Inject;
 import javax.ws.rs.QueryParam;
 
 
@@ -27,39 +32,37 @@ import javax.ws.rs.QueryParam;
 @Produces("application/json")
 public class EspecializacionResource
 {
-    /*
-    * Especializacion mock
-    */
-    private EspecializacionMock Especialidad = new EspecializacionMock();   
-    @GET
-	public List<EspecializacionDTO> getEspecializaciones() throws EspecializacionException
-	{
-		return Especialidad.getSpecialties();
-	}
-	
+
+    @Inject
+    private IEspecializacionLogic especialidad;
+    
     @GET
     @Path("{id: \\d+}")
-    public EspecializacionDTO getEspecializacionID(@PathParam("id")int id) throws EspecializacionException
+    public EspecializacionDetailDTO getEspecializacionID(@PathParam("id")int id) throws EspecializacionException
     {
-        return Especialidad.getSpecID(id);
+        EspecializacionEntity ent = especialidad.getEspecializacion((long)id);
+        return new EspecializacionDetailDTO(ent);
     }
 	
     @POST
-    public EspecializacionDTO createEspecialidad(EspecializacionDTO spec)throws EspecializacionException
+    public EspecializacionDetailDTO createEspecialidad(EspecializacionDetailDTO spec)throws EspecializacionException
     {
-     return Especialidad.createSpecialty(spec);
+     return new EspecializacionDetailDTO(spec.toEntity());
     }
     
     @DELETE
     @Path ("{id: \\d+}")
     public void deleteEspecializacion (@PathParam("id") int id) throws EspecializacionException
     {
-        Especialidad.deleteEspecializacion(id);
+        especialidad.deleteEspecializacion((long)id);
     }
     @PUT
     @Path ("{id: \\d+}")
-    public EspecializacionDTO updateEspecializacion (@PathParam("id") int id, EspecializacionDTO spec) throws EspecializacionException
+    public EspecializacionDetailDTO updateEspecializacion (@PathParam("id") int id, EspecializacionDetailDTO spec) throws EspecializacionException
     {
-        return Especialidad.updateEspecializacion(id, spec);
+        EspecializacionEntity entity = spec.toEntity();
+        entity.setId((long)id);
+        EspecializacionEntity oldEntity = especialidad.getEspecializacion((long)id);
+        return new EspecializacionDetailDTO(entity);
     }
 }
